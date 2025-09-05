af40da264aaba9e51f1a05758567ef3c353cb0cc
gmultiusuarios <168999005+gmultiusuarios@users.noreply.github.com>
2025-09-05 13:55:05 +0200

    Actualizacion

--- assets/LandingIntro-Dhkit7FV.js
+++
@@ -1,2 +0,0 @@
-import{j as l}from"./index-2dcVRJTl.js";function n(){return l.jsx("div",{className:"hero min-h-full rounded-l-xl bg-base-300",children:l.jsx("div",{className:"hero-content p-0 m-0 w-full h-full",children:l.jsx("img",{src:"/Login-02.png",alt:"logo",className:"object-cover w-full h-full rounded-l-xl hidden sm:block"})})})}export{n as L};
-//# sourceMappingURL=LandingIntro-Dhkit7FV.js.map
